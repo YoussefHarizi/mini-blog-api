e430def137750d7e069bd5b38b83b3c49cd085b2
Harizi <yharizi@gticanada.com>
2022-07-21 15:57:47 -0400

    work with omr-fixtures & facker to generate info in DB

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+     private UserPasswordHasherInterface $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $fake = Factory::create();
+
+        for ($u=0; $u < 10; $u++) {
+            $user = new User();
+
+            $passHash = $this->encoder->hashPassword($user, 'password');
+
+            $user->setEmail($fake->email)
+                ->setPassword($passHash);
+
+            $manager->persist($user);
+
+            for ($a = 0; $a < random_int(5, 15); $a++) {
+                $article = (new Article())->setAuthor($user)
+                    ->setContent($fake->text(300))
+                    ->setName($fake->text(50));
+
+                $manager->persist($article);
+            }
+        }
 
         $manager->flush();
     }
+
 }
